bff9eb925e628982e5f5f5113775754195dd0a4d
Robishu2 <robinb1804@gmail.com>
2026-02-03 15:48:11 +0100

    test

--- lambda_function.py
+++ lambda_function.py
@@ -1,31 +1,6 @@
 import json
 
 def lambda_handler(event, context):
-    # body = json.loads(event['body'])
-
-    # first_num = body['first_num']
-    # second_num = body['second_num']
-    # operator = body['operator']
-
-    # if operator == '+':
-    #     c = first_num + second_num
-    # elif operator == '-':
-    #     c = first_num - second_num
-    # elif operator == '*':
-    #     c = first_num * second_num
-    # else:
-    #     c = first_num / second_num
-
-    # print(f'{first_num} {operator} {second_num} = {c}')
-    # print('Version 2.0')
-
-    # # TODO implement
-    # return {
-    #     'statusCode': 200,
-    #     'headers': {"Content-type": "application/json"},
-    #     'body': json.dumps(c)
-    # }
-
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
