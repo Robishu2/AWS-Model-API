64d35874a484665d04fa202c6023fe14960fdf7b
Robishu2 <robinb1804@gmail.com>
2026-02-03 10:34:23 +0100

    change test code and remove pandas requirement

--- lambda_function.py
+++ lambda_function.py
@@ -1,27 +1,32 @@
 import json
 
 def lambda_handler(event, context):
-    body = json.loads(event['body'])
+    # body = json.loads(event['body'])
 
-    first_num = body['first_num']
-    second_num = body['second_num']
-    operator = body['operator']
+    # first_num = body['first_num']
+    # second_num = body['second_num']
+    # operator = body['operator']
 
-    if operator == '+':
-        c = first_num + second_num
-    elif operator == '-':
-        c = first_num - second_num
-    elif operator == '*':
-        c = first_num * second_num
-    else:
-        c = first_num / second_num
+    # if operator == '+':
+    #     c = first_num + second_num
+    # elif operator == '-':
+    #     c = first_num - second_num
+    # elif operator == '*':
+    #     c = first_num * second_num
+    # else:
+    #     c = first_num / second_num
 
-    print(f'{first_num} {operator} {second_num} = {c}')
-    print('Version 2.0')
+    # print(f'{first_num} {operator} {second_num} = {c}')
+    # print('Version 2.0')
 
-    # TODO implement
+    # # TODO implement
+    # return {
+    #     'statusCode': 200,
+    #     'headers': {"Content-type": "application/json"},
+    #     'body': json.dumps(c)
+    # }
+
     return {
         'statusCode': 200,
-        'headers': {"Content-type": "application/json"},
-        'body': json.dumps(c)
+        'body': json.dumps('Hello from Lambda!')
     }
